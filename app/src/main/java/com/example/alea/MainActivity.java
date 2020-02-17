74922ac91f65f6792d93b1bfe618b852cf1df7dc
aldryarief <aldreva@gmail.com>
2020-02-17 17:49:59 +0700

    post

--- app/src/main/java/com/example/alea/MainActivity.java
+++ app/src/main/java/com/example/alea/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.alea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String server_url = "https://aldry.000webhostapp.com/tampilSemuaPgw.php";
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.buttonView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error ...", Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError{
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("result", "Foo bars");
+                        return params;
+                    }
+
+                };
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
