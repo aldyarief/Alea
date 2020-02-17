a73ca4be7b13dfbfdef0a383f135afffc8eb1bab
aldryarief <aldreva@gmail.com>
2020-02-17 21:20:21 +0700

    coba

--- app/src/main/java/com/example/alea/MainActivity.java
+++ app/src/main/java/com/example/alea/MainActivity.java
@@ -18,17 +18,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String server_url = "https://aldry.000webhostapp.com/tampilSemuaPgw.php";
     Button button;
+    String text;
+    String server_url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.buttonView);
-
+        text = ((EditText) findViewById(R.id.textname)).getText().toString();
+        server_url = "https://aldry.000webhostapp.com/tampilSemuaPgw.php?name=$text";
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
