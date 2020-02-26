6700672d931c3f49d047ffee95151a425feb57b3
aldryarief <aldreva@gmail.com>
2020-02-26 17:24:54 +0700

    master user + layout

--- app/src/main/java/com/example/alea/Dashboard.java
+++ app/src/main/java/com/example/alea/Dashboard.java
@@ -2,13 +2,49 @@ package com.example.alea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
-
+    private TextView txuser;
+    private ImageView imageuser;
+    private RelativeLayout layoutuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        txuser = (TextView) findViewById(R.id.txUser);
+        imageuser = (ImageView) findViewById(R.id.imageuser);
+        layoutuser = (RelativeLayout) findViewById(R.id.layoutuser);
+
+        txuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicit = new Intent(Dashboard.this, MasterUser.class);
+                startActivity(explicit);
+            }
+        });
+
+        imageuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicit = new Intent(Dashboard.this, MasterUser.class);
+                startActivity(explicit);
+            }
+        });
+
+        layoutuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicit = new Intent(Dashboard.this, MasterUser.class);
+                startActivity(explicit);
+            }
+        });
     }
 }
