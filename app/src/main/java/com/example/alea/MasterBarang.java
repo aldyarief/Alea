49767bb945bc030290158dad7bbe106731f1fd30
aldryarief <aldreva@gmail.com>
2020-05-11 16:19:45 +0700

    master barang

--- app/src/main/java/com/example/alea/MasterBarang.java
+++ app/src/main/java/com/example/alea/MasterBarang.java
@@ -2,8 +2,11 @@ package com.example.alea;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -24,12 +27,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
+import android.view.View;
+import android.widget.TextView;
 
-public class MasterBarang extends AppCompatActivity {
+public class MasterBarang extends AppCompatActivity implements Spinner.OnItemSelectedListener {
     ProgressDialog pd;
-    String server_url,data;
+    String server_url,data,id,nama,harga,katid,userid;
     Spinner spinnerKategori;
+    EditText Ednama,Edharga;
     String JSON_ARRAY = "data";
+    Button btnsave,btntutup;
+    TextView DataID;
+    private TextView textID;
     private ArrayList<String> kategori;
     private JSONArray result;
 
@@ -40,9 +49,46 @@ public class MasterBarang extends AppCompatActivity {
         setContentView(R.layout.activity_master_barang);
         pd = new ProgressDialog(this);
         spinnerKategori = (Spinner) findViewById(R.id.Spinner);
+        textID = (TextView) findViewById(R.id.textID);
+        btnsave = (Button) findViewById(R.id.btnSave);
+        btntutup = (Button) findViewById(R.id.btnTutup);
+        Ednama = (EditText) findViewById(R.id.ednambar);
+        Edharga = (EditText) findViewById(R.id.edharbar);
+        DataID = (TextView) findViewById(R.id.textID);
+        userid = (getIntent().getStringExtra("userid"));
         server_url = "https://aldry.000webhostapp.com/showkategori.php";
         kategori = new ArrayList<String>();
         AmbilKategori();
+        spinnerKategori.setOnItemSelectedListener(this);
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String nama = Ednama.getText().toString().trim();
+            String harga = Edharga.getText().toString().trim();
+            String katid = DataID.getText().toString().trim();
+
+            if (!nama.isEmpty() && !harga.isEmpty() ) {
+                simpanData(nama,harga,katid,userid);
+            } else if (nama.isEmpty()) {
+                Ednama.setError("nama barang tidak boleh kosong");
+                Ednama.requestFocus();
+            } else if (harga.isEmpty()) {
+                Edharga.setError("harga barang tidak boleh kosong");
+                Edharga.requestFocus();
+            }
+            }
+
+        });
+
+        btntutup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicit = new Intent(MasterBarang.this, Barang.class);
+                startActivity(explicit);
+            }
+
+        });
     }
 
     private void AmbilKategori() {
@@ -103,6 +149,82 @@ public class MasterBarang extends AppCompatActivity {
         ArrayAdapter dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, kategori);
         dataAdapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
         spinnerKategori.setAdapter(dataAdapter);
+    }
+
+    private String getKategoriID(int position){
+        String KategoriID="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            KategoriID = json.getString("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return KategoriID;
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Setting the values to textviews for a selected item
+        textID.setText(getKategoriID(position));
+    }
+
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        textID.setText("");
+    }
+
+    private void simpanData(final String nama,final String harga,final String katid,final String userid) {
+        final RequestQueue requestQueue = Volley.newRequestQueue(MasterBarang.this);
+
+        pd.setCancelable(false);
+        pd.setMessage("Harap Menunggu...");
+        showDialog();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("response", response.toString());
+                        hideDialog();
+
+                        try {
+                            JSONObject jObject = new JSONObject(response);
+                            String pesan = jObject.getString("pesan");
+                            String hasil = jObject.getString("result");
+                            if (hasil.equalsIgnoreCase("true")) {
+                                Toast.makeText(MasterBarang.this,pesan, Toast.LENGTH_SHORT).show();
+                                requestQueue.stop();
+                                Ednama.getText().clear();
+                                Edharga.getText().clear();
+
+                            } else {
+                                Toast.makeText(MasterBarang.this, pesan, Toast.LENGTH_SHORT).show();
+                                requestQueue.stop();
+                                Ednama.getText().clear();
+                                Edharga.getText().clear();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MasterBarang.this, "Error JSON", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("ERROR", error.getMessage());
+                Toast.makeText(MasterBarang.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("nama", nama);
+                param.put("harga", harga);
+                param.put("katid", katid);
+                return param;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 
     private void showDialog() {
